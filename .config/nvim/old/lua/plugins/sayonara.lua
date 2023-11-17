9ea46fc0de89df9c41f0c6d3270c197e1757b09d
Laurens Rosinski <rosinski@duck.com>
2023-11-17 20:29:15 +0100

    Deleted 'old' files

--- .config/nvim/old/lua/plugins/sayonara.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"mhinz/vim-sayonara", cmd = "Sayonara",
-	keys = {
-		{ '<C-q>', vim.cmd.Sayonara, desc = "Quit the buffer." },
-		{ '<C-e>', "<cmd>Sayonara!<CR>", desc = "Quit the buffer, but don't close vim." },
-		{ 'ZZ', vim.cmd.w and vim.cmd.Sayonara, desc = "Save and close the buffer." },
-	},
-}
