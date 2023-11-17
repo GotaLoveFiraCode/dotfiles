d3b4d18023a143d91cd55d13ee3e7eb875eb7ccc
Laurens Rosinski <rosinski@duck.com>
2023-11-17 19:43:05 +0100

    Added all my plugins, finished heirline config

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -72,5 +72,20 @@ vim.keymap.set({'n', 'v'}, '<C-p>', '"+')
 vim.keymap.set('n', '<leader>y', '0D')
 vim.keymap.set('n', '0', '^')
 vim.keymap.set('n', '^', '0')
+vim.keymap.set('n', 'ga', ':')
+vim.keymap.set('n', 'g-', ':')
 -- }}}
 
+-- for front-end neovide, nvim ignores this
+if vim.g.neovide then
+	vim.o.guifont = "JetBrainsMono Nerd Font:h14:#e-subpixelantialias:#h-slight"
+	vim.opt.linespace = 1
+	-- vim.g.neovide_padding_top = 4
+	vim.g.neovide_floating_blur_amount_x = 2.0
+	vim.g.neovide_floating_blur_amount_y = 2.0
+	vim.g.neovide_hide_mouse_when_typing = true
+	vim.g.neovide_theme = 'dark'
+	vim.g.neovide_refresh_rate = 144
+	vim.g.neovide_cursor_antialiasing = true
+	vim.g.neovide_cursor_vfx_mode = "pixiedust"
+end
