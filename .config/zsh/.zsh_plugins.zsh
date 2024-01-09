aee3859531c270823d575315b25e6bca31a5158d
Laurens Rosinski <rosinski@duck.com>
2024-01-09 12:01:49 +0100

    fix(mostly zsh): added missing files

---
+++ .config/zsh/.zsh_plugins.zsh
@@ -0,0 +1,30 @@
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-clarketm-SLASH-zsh-completions )
+source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-clarketm-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/completion )
+source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/completion/completion.plugin.zsh
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/editor )
+source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/editor/editor.plugin.zsh
+if ! (( $+functions[zsh-defer] )); then
+  fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer )
+  source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer/zsh-defer.plugin.zsh
+fi
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-atuinsh-SLASH-atuin )
+zsh-defer source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-atuinsh-SLASH-atuin/atuin.plugin.zsh
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-auto-notify )
+zsh-defer source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-auto-notify/auto-notify.plugin.zsh
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair )
+zsh-defer source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair/autopair.plugin.zsh
+zsh-defer source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair/zsh-autopair.plugin.zsh
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
+zsh-defer source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-davidde-SLASH-git )
+zsh-defer source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-davidde-SLASH-git/git.plugin.zsh
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zfunctions )
+zsh-defer source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zfunctions/zfunctions.plugin.zsh
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
+source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
+source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
+source /home/ltr/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
